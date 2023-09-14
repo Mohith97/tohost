4186398bb14470372d0319762782aa15ca25a156
Mohith97 <106798968+Mohith97@users.noreply.github.com>
2023-09-14 11:40:23 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tohost.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
